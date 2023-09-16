ad1eefc32214765e65ba1bf2d6dbff31766e31aa
Chalermpong Dejnarong <chalermpong.d@arise.tech>
2023-09-16 13:18:02 +0700

    Try to Link component

--- coffee-app/pages/greeting/smile/[id].tsx
+++ coffee-app/pages/greeting/smile/[id].tsx
@@ -1,9 +1,19 @@
 import styles from "@/styles/greeting.module.css";
+import Link from "next/link";
 import { useRouter } from "next/router";
 
 // http://localhost:3000/greeting/smile/{id}
 const Smile = () => {
   const router = useRouter();
-  return <div className={styles.container}>Smile by {router.query.id}</div>;
+  return (
+    <div className={styles.container}>
+      Smile by {router.query.id}
+      <div>
+        <Link href="/" passHref>
+          Back to home
+        </Link>
+      </div>
+    </div>
+  );
 };
 export default Smile;
