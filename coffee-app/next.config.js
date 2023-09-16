9f4bb82a9d4daa2de3ba31a606b24ce592711547
Chalermpong Dejnarong <chalermpong.d@arise.tech>
2023-09-16 16:43:34 +0700

    how to list card and calling image from another host

--- coffee-app/next.config.js
+++ coffee-app/next.config.js
@@ -1,6 +1,9 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   swcMinify: true,
+  images: {
+    domains: ["assets.example.com"], // for calling image from another host
+  },
 };
 
 module.exports = nextConfig;
--- coffee-app/pages/home/index.tsx
+++ coffee-app/pages/home/index.tsx
@@ -1,37 +1,33 @@
 import Card from "@/components/Card";
-import { wrap } from "module";
+import styles from "@/styles/card.module.css";
+import coffeeStores from "@/data/coffee.json";
 
 const Home = () => {
+  interface CoffeeItem {
+    id: number;
+    name: string;
+    imgUrl: string;
+  }
+  const coffeeItems: JSX.Element[] = coffeeStores.map((item: CoffeeItem) => {
+    return (
+      <Card href={`/coffee/${item.id}`} name={item.name} imgUrl={item.imgUrl} />
+    );
+  });
+
   return (
     <div>
       <h1>Home Page</h1>
       <div
-        style={{
-          display: "flex",
-          flexWrap: "wrap",
-          backgroundColor: "DodgerBlue",
-        }}
+        className={styles.card_container}
+        style={
+          {
+            // display: "flex",
+            // flexWrap: "wrap",
+            // backgroundColor: "DodgerBlue",
+          }
+        }
       >
-        <Card
-          href="/coffee/latte"
-          name="Latte light milk"
-          imgUrl="/images/product.jpeg"
-        />
-        <Card
-          href="/coffee/latte"
-          name="Latte light milk"
-          imgUrl="/images/product.jpeg"
-        />
-        <Card
-          href="/coffee/latte"
-          name="Latte light milk"
-          imgUrl="/images/product.jpeg"
-        />
-        <Card
-          href="/coffee/latte"
-          name="Latte light milk"
-          imgUrl="/images/product.jpeg"
-        />
+        {coffeeItems}
       </div>
     </div>
   );
